d326ddbcd7e23b8d126b7c0e57ad279ee6918295
Bonyfachel <71444442+Bonyfachel@users.noreply.github.com>
2020-12-10 08:45:00 +0400

    Add files via upload

---
+++ Situation.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Situation {
+
+    public Situation[] direction;
+    public String subject, text;
+    public int dK, dA, dR;
+
+    public Situation(String subject, String text,
+                     int variants, int dK, int dA,
+                     int dR) {
+        this.subject = subject;
+        this.text = text;
+        this.dK = dK;
+        this.dA = dA;
+        this.dR = dR;
+        direction = new Situation[variants];
+    }
+}
+
